fdca78acf302fd6dcb38c874e235bc31d7195ac3
MBPrdctns <mike.baumann1@web.de>
2024-02-28 14:58:25 +0100

    added game against first model

---
+++ scripts/random_rollout_gui.py
@@ -0,0 +1,50 @@
+import gymnasium as gym
+# import minihex
+import numpy as np
+from sb3_contrib.common.maskable.policies import MaskableActorCriticPolicy
+from sb3_contrib.common.wrappers import ActionMasker
+from sb3_contrib.ppo_mask import MaskablePPO
+from minihex.interactive.interactive import InteractiveGame
+from configparser import ConfigParser
+
+
+def mask_fn(env: gym.Env) -> np.ndarray:
+    return env.get_action_mask()
+
+
+config = ConfigParser()
+config.read('config.ini')
+
+env = gym.make("hex-v0",
+               opponent_policy="interactive",
+               board_size=11, show_board=True)
+env = ActionMasker(env, mask_fn)
+model = MaskablePPO.load("hex")
+
+state, info = env.reset()
+terminated = False
+# interactive = InteractiveGame(config, env)
+
+while not terminated:
+    print("while")
+    # breakpoint()
+    board = state
+    # breakpoint()
+    # interactive.board = board
+    # interactive.gui.update_board(board)
+    # action = interactive.play_move()
+    # print(action)
+    # action = env.simulator.coordinate_to_action(action)
+    # winner = env.simulator.fast_move(action)
+    action, _states = model.predict(state, deterministic=True, action_masks=env.get_action_mask())
+    state, reward, terminated, truncated, info = env.step(action) # minihex.random_policy(board, player, info)
+    # state, reward, done, info = env.step(action)
+
+env.render()
+
+if reward == -1:
+    print("Player (Black) Lost")
+elif reward == 1:
+    print("Player (Black) Won")
+else:
+    print("Draw")
