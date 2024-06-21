37d72632e382fdecc33ef4e76313edfc1073ee5c
lucamax32 <Luca.maximini@gmail.com>
2024-06-21 16:07:47 +0200

    fix setup file

--- setup.py
+++ setup.py
@@ -4,15 +4,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="minihex",
-    version="1.0.1",
-    author="Sebastian Wallkotter",
-    author_email="sebastian@wallkoetter.net",
+    name="hex_gym_env",
+    version="1.0",
     description=("The game of Hex implemented for reinforcement learning in"
-                 " the OpenAI gym framework. Optimized for rollout speed."),
+                 " the OpenAI gym framework."),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/FirefoxMetzger/minihex.git",
+    url="https://github.com/MBPrdctns/hex_gym_env.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -22,6 +20,9 @@ setuptools.setup(
     python_requires='>=3.7',
     install_requires=[
         'numpy>=1.18.2',
-        'gym>=0.17.1'
+        'gym>=0.17.1',
+        'gymnasium',
+        "sb3_contrib",
+        "pygame"
     ]
 )
